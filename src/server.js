4f5a512e6218caee5f15b9a54c466770698be46c
Noorb7188 <noorb7188@gmail.com>
2019-04-24 13:35:28 +0300

    test heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const hostname = process.env.HOSTNAME || 'localhost';
-const port = process.env.PORT || 4000;
+const hostname = process.env.HOST || 'localhost';
+const port = process.env.PORT || 5000;
 const router = require('./router')
 
 http.createServer(router).listen(port, hostname, () => {
