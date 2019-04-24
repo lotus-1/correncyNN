1840ea4aa35d4a247d0218615b682a567d6fd77c
NaremanPro <violette1988@gmail.com>
2019-04-24 10:44:31 +0300

    having passing server tests

--- tests/test.js
+++ tests/test.js
@@ -7,6 +7,13 @@ test('Tape is working', (t) => {
   t.end();
 });
 
+test('Fetch function is working', (t) => {
+  const expected = 'string';
+  const actual = typeOf (fetchData(str));
+  t.equal(actual, expected, 'Should get a string when given a string');
+  t.end();
+});
+
 test('Home route returns a status code of 200', (t) => {
   supertest(router)
     .get('/')
@@ -15,6 +22,30 @@ test('Home route returns a status code of 200', (t) => {
     .end((err, res) => {
       t.error(err);
       t.equal(res.statusCode, 200, 'Should return 200');
+      t.end();
+    });
+});
+
+test('Public route returns a status code of 200', (t) => {
+  supertest(router)
+    .get('/public')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.equal(res.statusCode, 200, 'Should return 200');
+      t.end();
+    });
+});
+
+test('Convert route returns the currency value of the input currency', (t) => {
+  supertest(router)
+    .get('/convert?cur=USD')
+    .expect('3.5997332148')
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.equal(res.text, '3.5997332148', 'Should return 3.5997332148 when entering USD');
       t.end();
     });
 });
