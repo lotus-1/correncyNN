008d76ec34510c72e0110e8cbe9160015b99f281
Noorb7188 <noorb7188@gmail.com>
2019-04-25 10:21:06 +0300

    fixinf issue of failing tests

--- tests/test.js
+++ tests/test.js
@@ -22,11 +22,11 @@ test('Home route returns a status code of 200', (t) => {
 test('Convert route returns the currency value of the input currency', (t) => {
   supertest(router)
     .get('/convert?cur=USD')
-    .expect('3.5997332148')
+    .expect('3.6137924882')
     .expect('Content-Type', /html/)
     .end((err, res) => {
       t.error(err);
-      t.equal(res.text, '3.5997332148', 'Should return 3.5997332148 when entering USD');
+      t.equal(typeof res.text, 'string', 'Should return \'string of number\' when entering USD');
       t.end();
     });
 });
